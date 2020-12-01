c6dee4a2c22dc71e5b66660c8c4b7b3c6a40867a
Erika Forslöf <erikaforslov@hotmail.com>
2020-12-01 06:48:23 +0100

    Fixed ints

--- day01/part1.py
+++ day01/part1.py
@@ -2,9 +2,9 @@ import itertools
 list = set()
 with open('input.txt') as input:
 	for i in input.readlines():
-		list.add(i.rstrip())
+		list.add(int(i))
 
 for a, b in itertools.combinations(list, 2):
-    if int(a) + int(b) == 2020:
-        print(int(a) * int(b))
+    if a + b == 2020:
+        print(a * b)
 
--- day01/part2.py
+++ day01/part2.py
@@ -2,14 +2,14 @@ import itertools
 list = set()
 with open('input.txt') as input:
 	for i in input.readlines():
-		list.add(i.rstrip())
+		list.add(int(i))
 
 for a, b in itertools.combinations(list, 2):
-    if int(a) + int(b) < 2020:
-        one = int(a) + int(b)
-        sumA = int(a)
-        sumB = int(b)
+    if a + b < 2020:
+        one = a + b
+        sumA = a
+        sumB = b
         for c in list:
-            if one + int(c) == 2020:
-                print(int(c) * sumA * sumB)
+            if one + c == 2020:
+                print(c * sumA * sumB)
                 quit()
