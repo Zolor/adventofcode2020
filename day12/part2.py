83a772aca3b419c8575bc8d71837510450b2bf47
Erika Forslöf <erikaforslov@hotmail.com>
2020-12-12 15:22:39 +0100

    Small fixes day 12

--- day12/part2.py
+++ day12/part2.py
@@ -2,7 +2,6 @@ navigation = open("input.txt").read().split("\n")
 
 waypoint = {"N":1, "E":10, "S":0, "W":0}
 location = {"N":0, "E":0, "S":0, "W":0}
-nav = ["N", "E", "S", "W"]
 
 for i in navigation:
     direction = i[0]
@@ -21,12 +20,10 @@ for i in navigation:
                 waypoint["E"] = waypoint.get("S")
                 waypoint["S"] = waypoint.get("W")
                 waypoint["W"] = tmp
-    elif direction in nav:
+    elif direction in location:
         waypoint[direction] += int(i[1:])
     elif i[0] == "F":
         for k, v in waypoint.items():
             location[k] += int(i[1:]) * v
 
-longitude = abs(location.get("N") - location.get("S"))
-latitude = abs(location.get("W") - location.get("E"))
-print(latitude + longitude)
+print(abs(location.get("W") - location.get("E")) + abs(location.get("N") - location.get("S")))
