5739c66a91c03a563c90073a84fdf898a074994c
Erika Forslöf <erikaforslov@hotmail.com>
2020-12-21 00:09:53 +0100

    Day19 part2 done!

--- day19/part2.py
+++ day19/part2.py
@@ -1,4 +1,3 @@
-import functools
 import re
 
 data = open("input_part2.txt").read().split("\n")
@@ -14,34 +13,44 @@ for line in data:
     elif istrue:
         messages.append(line)
     else:
-        k, v = line.split(": ")
-        k = int(k)
-        if v[0] == '"':
-            v = v[1]
-            dct_rules[k] = [v]
-            continue
-        tmp_list = []
-        for item in v.split('|'):
-            tmp_list.append(tuple(map(int, item.split())))
-        dct_rules[k] = tmp_list
+        k, _, v = line.partition(": ")
+        dct_rules[k] = v
 
-def gen_regex(rules, start):
-    @functools.lru_cache()
-    def rec(start):
-        parts = []
-        for item in rules[start]:
-            if isinstance(item, str):
-                parts.append(item)
-            else:
-                parts.append(''.join(rec(i) for i in item))
-        if len(parts) == 1:
-            return parts[0]
-        return '(?:' + '|'.join(parts) + ')'
-    return rec(start)
+def gen_regex(rules, messages):
+    def get_regex(s):
+        if s == '|':
+            return s
 
-validator = re.compile('^' + gen_regex(dct_rules, 0) + "$")
-for item in messages:
-    if bool(re.search(validator, item)):
-        summa += 1
+        rule = dct_rules[s]
+        if rule.startswith('"'):
+            return rule.strip('"')
+        else:
+            return f'({"".join(get_regex(part) for part in rule.split())})'
+    
+    reg_42 = re.compile(get_regex("42"))
+    reg_31 = re.compile(get_regex("31"))
 
-print(summa)
+    count = 0
+    for message in messages:
+        pos = 0
+
+        count_42 = 0
+        match = reg_42.match(message, pos)
+        while match:
+            count_42 += 1
+            pos = match.end()
+            match = reg_42.match(message, pos)
+        
+        count_31 = 0
+        match = reg_31.match(message, pos)
+        while match:
+            count_31 += 1
+            pos = match.end()
+            match = reg_31.match(message, pos)
+
+        if pos == len(message) and 0 < count_31 < count_42:
+            count += 1
+    
+    return count
+            
+print(gen_regex(dct_rules, messages))
