2f5a292bb0262e80c83f2d6ce465c867acd5747a
Erika Forslöf <erikaforslov@hotmail.com>
2020-12-23 16:51:28 +0100

    Day 23 part 2 complete!

--- day23/part2.py
+++ day23/part2.py
@@ -3,51 +3,42 @@ data = open("input.txt").read()
 clock = [int(i) for i in data]
 for x in range(max(clock) + 1, 1000000 + 1):
     clock.append(x)
+first = clock[0]
+last = clock[-1]
+clock_dict = {clock[k-1]:clock[k] for k in range(1, len(clock))}
+clock_dict[last] = first
 
-cups = clock
-curr_cup_index = 0
+# dict[key=current cup value] = value = next cup value
+
+curr_cup_key = clock[0]
 move = 1
 
 for l in range(10000000):
     pick_up = []
     #Pick up the 3 cups
-    pop_index = curr_cup_index + 1
-    curr_cup = clock[curr_cup_index]
-    for cup in range(3):
-        if pop_index > (len(clock) - 1):
-            pick_up.append(clock.pop(0))
-        else:
-            pick_up.append(clock.pop(pop_index))
-    pick_up.reverse()
-    dest_cup = curr_cup_index
-    counter = 1
-    #Find where to put our 3 cups and place accordingly
-    while True:
-        dest_cup = curr_cup - counter
-        if dest_cup < 0:
-            dest_cup = max(clock)
-        if dest_cup in clock:
-            for cup in pick_up:
-                clock.insert(clock.index(dest_cup) + 1, cup)
+    #Save pointer number for third ahead
+    third_pointer = clock_dict[clock_dict[clock_dict[clock_dict[curr_cup_key]]]]
+    #Save a list of our values so we can make sure to not jump to those
+    tmp_lista = [clock_dict[curr_cup_key],clock_dict[clock_dict[curr_cup_key]],clock_dict[clock_dict[clock_dict[curr_cup_key]]]]
+    #Save pointer for the value our number is headed, make sure our number exists and is not in our lista
+    lookahead_key = 0
+    for counter in range(1,5):
+        tmp_key = curr_cup_key - counter
+        if tmp_key < 1:
+            tmp_key += len(clock)
+        if tmp_key not in tmp_lista:
+            lookahead_key = tmp_key
             break
-        counter += 1
-    #Make sure to turn our list like a clock
-    if curr_cup_index != clock.index(curr_cup):
-        clock = clock[clock.index(curr_cup)-curr_cup_index:] + clock[:clock.index(curr_cup)-curr_cup_index]
-    curr_cup_index += 1
-    move += 1
-    #Make sure we don't overflow our list index
-    if curr_cup_index >= len(clock):
-        curr_cup_index = 0
+    lookahead_pointer = clock_dict[lookahead_key]
+    #Repoint headed number to start of our three
+    clock_dict[lookahead_key] = clock_dict[curr_cup_key]
+    #Repoint last of our three to our saved value
+    clock_dict[clock_dict[clock_dict[clock_dict[curr_cup_key]]]] = lookahead_pointer
+    #Repoint current to value of 3 ahead
+    clock_dict[curr_cup_key] = third_pointer
+    curr_cup_key = clock_dict[curr_cup_key]
 
-resultat = ""
-ind = clock.index(1)
-for x in clock:
-    if clock[ind] == 1:
-        ind += 1
-        continue
-    resultat += str(clock[ind])
-    ind += 1
-    if ind > len(clock) - 1:
-        ind = 0
-print(resultat)
+first = clock_dict[1]
+second = clock_dict[clock_dict[1]]
+
+print(first * second)
--- day23/test.py
+++ day23/test.py
@@ -1,4 +1,23 @@
-data = open("testinput.txt").read()
+data = open("input.txt").read()
+
+test = {'1': '8', '8': '6', '6': '5', '5': '2', '2': '4', '4': '9', '9': '7', '7': '3'}
+
+third_pointer = test[test[test[test["1"]]]]
+tmp_lista = [test["1"],test[test["1"]],test[test[test["1"]]]]
+print(third_pointer)
+print(tmp_lista)
+
+""" 
+clock = [int(i) for i in data]
+for x in range(max(clock) + 1, 10 + 1):
+    clock.append(x)
+first = clock[0]
+last = clock[-1]
+clock_dict = {clock[k-1]:clock[k] for k in range(1, len(clock))}
+clock_dict[last] = first
+
+print(clock_dict)
+
 
 clock = [int(i) for i in data]
 print(clock)
@@ -7,7 +26,6 @@ for x in range(max(clock) + 1, 1000000 + 1):
 
 print(max(clock))
 
-""" 
 pick_up = []
 curr_cup = 0
 start = curr_cup + 1
