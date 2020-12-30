61d46ee376b2fd05e26f998d5eb100ad14d9add2
Erika Forslöf <erikaforslov@hotmail.com>
2020-12-31 00:33:28 +0100

    Day 20 part 2 complete!
    2020 is a wrap!

--- day20/part2.py
+++ day20/part2.py
@@ -1,6 +1,10 @@
-import collections
+import time
+start_time = time.time()
 
-data = open("testinput.txt").read().split("\n")
+import collections
+import re
+
+data = open("input.txt").read().split("\n")
 
 tile_dct = {}
 #Dict containing connections
@@ -21,16 +25,29 @@ class piece:
     
     def get_content(self):
         return(self.content)
+
+    def get_content_without_sides(self):
+        tmp_img = []
+        for line in self.content[1:-1]:
+            tmp_img.append(line[1:-1])
+        return(tmp_img)
     
     def get_sides(self):
         #Return a list of all corners
         return([self.top, self.right, self.bottom, self.left])
 
-    def flipper(self):
+    def h_flipper(self):
         #Take a list of strings and return it flipped
         tmp_img = []
         for line in self.content:
             tmp_img.append(''.join(reversed(line)))
+        self.update_image(tmp_img)
+
+    def v_flipper(self):
+        #Take a list of strings and return it flipped
+        tmp_img = []
+        for line in reversed(self.content):
+            tmp_img.append(''.join(line))
         self.update_image(tmp_img)
 
     def rotate(self):
@@ -57,7 +74,7 @@ for k, v in tile_dct.items():
             if k == k2:
                 continue
             for side2 in v2.get_sides():
-                flipped_side2 = side2[len(side2)::-1]
+                flipped_side2 = ''.join(reversed(side2)) #side2[len(side2)::-1]
                 if flipped_side2 == side:
                     matches[k].update({k2 : side2})
                 elif side2 == side:
@@ -79,29 +96,29 @@ for counter in range(15):
     start_corner_sides = tile_dct[start_corner].get_sides()
     if start_corner_sides[1] in [one, two, ''.join(reversed(one)), ''.join(reversed(two))]:
         if start_corner_sides[2] in [one, two, ''.join(reversed(one)), ''.join(reversed(two))]:
-            image = tile_dct[start_corner].get_content()
+            image = tile_dct[start_corner].get_content_without_sides()
             right_side = start_corner_sides[1]
             down_side = start_corner_sides[2]
             break
     if counter == 4:
-        tile_dct[start_corner].flipper()
+        tile_dct[start_corner].h_flipper()
     elif counter == 8:
-        tile_dct[start_corner].flipper()
+        tile_dct[start_corner].h_flipper()
         tile_dct[start_corner].rotate()
     elif counter == 9:
-        tile_dct[start_corner].flipper()
+        tile_dct[start_corner].h_flipper()
     else:
         tile_dct[start_corner].rotate()
 
 #Find our next piece and rotate it correctly towards our previous piece
 prev_piece = start_corner
-print(prev_piece)
 curr_piece = [k for k,v in matches.get(prev_piece).items() if v == right_side or v == ''.join(reversed(right_side))][0]
 count = 0
 line = 0
 corner = False
+pieces_added = 1
 
-while count < 9:
+while True:
     correct_index = 0
     sides = tile_dct[curr_piece].get_sides()
     reverse = None
@@ -116,39 +133,95 @@ while count < 9:
     if turns > 0:
         for x in range(turns):
             tile_dct[curr_piece].rotate()
+
+    sides = tile_dct[curr_piece].get_sides()
     if right_side in sides:
         reverse = False
     elif ''.join(reversed(right_side)) in sides:
-        reverse = False
+        reverse = True
     if reverse == True:
-        tile_dct[curr_piece].flipper()
+        if corner:
+            tile_dct[curr_piece].h_flipper()
+        else:
+            tile_dct[curr_piece].v_flipper()
         reverse = False
     #Append to image
     if corner:
-        image.append("")
+        #image.append("")
         line = len(image)
-        for tile_line in tile_dct[curr_piece].get_content():
+        for tile_line in tile_dct[curr_piece].get_content_without_sides():
+        #for tile_line in tile_dct[curr_piece].get_content_without_sides():
             image.append(tile_line)
         down_side = tile_dct[curr_piece].get_sides()[2]
         corner = False
+        pieces_added += 1
     else:
-        for num, tile_line in enumerate(tile_dct[curr_piece].get_content()):
-            image[line + num] += " " + tile_line
-    for image_line in image:
-        print(image_line)
-    print("LOL")
+        for num, tile_line in enumerate(tile_dct[curr_piece].get_content_without_sides()):
+        #for num, tile_line in enumerate(tile_dct[curr_piece].get_content_without_sides()):
+            image[line + num] += tile_line
+        pieces_added += 1
 
     prev_piece = curr_piece
     right_side = tile_dct[prev_piece].get_sides()[1]
     #Check if we've reached corner
     if len([k for k,v in matches.get(prev_piece).items() if v == right_side or v == ''.join(reversed(right_side))]) == 0:
         prev_piece = start_corner
-        curr_piece = [k for k,v in matches.get(prev_piece).items() if v == down_side or v == ''.join(reversed(down_side))][0]
-        start_corner = curr_piece
-        right_side = tile_dct[prev_piece].get_sides()[2]
-        corner = True
+        if len([k for k,v in matches.get(prev_piece).items() if v == down_side or v == ''.join(reversed(down_side))]) != 0:
+            curr_piece = [k for k,v in matches.get(prev_piece).items() if v == down_side or v == ''.join(reversed(down_side))][0]
+            start_corner = curr_piece
+            right_side = tile_dct[prev_piece].get_sides()[2]
+            corner = True
+        else:
+            break
     else:
         prev_piece = curr_piece
         right_side = tile_dct[prev_piece].get_sides()[1]
         curr_piece = [k for k,v in matches.get(prev_piece).items() if v == right_side or v == ''.join(reversed(right_side))][0]
     count += 1
+
+image = piece(image)
+
+#Picture Complete!? Time to find seamonsters!
+sea_monster_hash = 15
+
+head_regex = re.compile("(?=[.#]{18}#[.#]{1})")
+mid_regex = re.compile("#[.#]{4}[#]{2}[.#]{4}[#]{2}[.#]{4}[#]{3}")
+bot_regex = re.compile("[.#]{1}#[.#]{2}#[.#]{2}#[.#]{2}#[.#]{2}#[.#]{2}#[.#]{3}")
+
+def find_seamonsters(img):
+    sea_monsters = 0
+    tmp_img = img.get_content()
+    for n in range(len(tmp_img) - 1):
+        for match in head_regex.finditer(tmp_img[n]):
+            if mid_regex.match(tmp_img[n+1], match.start()) and bot_regex.match(tmp_img[n+2], match.start()):
+                sea_monsters += 1
+    return sea_monsters
+
+counter = 0
+while True:
+    found_monsters = find_seamonsters(image)
+    if found_monsters == 0:
+        image.rotate()
+        if counter == 4:
+            image.h_flipper()
+        elif counter == 8:
+            image.v_flipper()
+        elif counter == 12:
+            image.h_flipper()
+            image.v_flipper()
+    else:
+        hashtags = 0
+        for row in image.get_content():
+            for x in row:
+                if x == "#":
+                    hashtags += 1
+        print(hashtags - (found_monsters * sea_monster_hash))
+        break
+    if counter > 15:
+        break
+    counter += 1
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#2136 Too low
+#2586 Too high
--- day20/test.py
+++ day20/test.py
@@ -1,10 +1,20 @@
-img = ['#..#','..##','.###','..#.#']
+import re
 
-for line in ([''.join(list(i)[::-1]) for i in zip(*img)]):
-    print(line)
+data = open("testdragon.txt").read().split("\n")
 
-x = "ABC"
-y = "DEF"
+sea = [l for l in data]
 
-for a, b in zip(img, img):
-    print(a + b)
+head_regex = re.compile("(?=..................#.)")
+mid_regex = re.compile("#[.#]{4}[#]{2}[.#]{4}[#]{2}[.#]{4}[#]{3}")
+bot_regex = re.compile("[.#]{1}#[.#]{2}#[.#]{2}#[.#]{2}#[.#]{2}#[.#]{2}#[.#]{3}")
+
+sea_monsters = 0
+for n, s in enumerate(sea):
+    if n == len(s) - 1:
+        continue
+    else:
+        for match in head_regex.finditer(s):
+            print(match.start)
+            if mid_regex.match(sea[n+1], match.start()) and bot_regex.match(sea[n+2], match.start()):
+                sea_monsters += 1
+print(sea_monsters)
